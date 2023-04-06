556f9cd1d8ca778c658d101fcade787c80528d3e
princessempel <princess.empel@gmail.com>
2023-04-06 13:15:09 -0400

    starter files

---
+++ EmpathyBytesSkeletons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EmpathyBytesSkeletons
+//
+//  Created by Princess Empel on 4/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EmpathyBytesSkeletons/EmpathyBytesSkeletonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmpathyBytesSkeletonsApp.swift
+//  EmpathyBytesSkeletons
+//
+//  Created by Princess Empel on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct EmpathyBytesSkeletonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
